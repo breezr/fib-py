20ca402727f396cde1dc48b88d29c179e72b0d87
Andrew Pan <ycpan711@gmail.com>
2025-03-13 17:23:32 +0900

    adding fib code

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@
 # use find_packages to exclude tests.
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup (
     name="andrew_fib_py",
     version="0.0.1",
     author="Andrew Pan",
     author_email="ycpan711@gmail.com",
     description="Calculate fibonacci numbers",
-    long_description="A basic package for calculating fibonacci numbers.",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/breezr/fib-py",
     install_requires=[],
